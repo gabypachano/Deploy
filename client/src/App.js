041087db251cc97641823dbbf0667e49c39accab
gabypachano <gabrielapachano.13@gmail.com>
2024-01-30 23:44:47 -0300

    new url backend

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Landing from "./views/Landing/Landing";
 import NavBar from "./components/NavBar/NavBar";
 import "./App.css";
 import axios from "axios";
-axios.defaults.baseURL = "https://deploy-production-501a.up.railway.app/";
+axios.defaults.baseURL = "https://deploy-production-ce0d.up.railway.app/";
 // Que haga las peticiones a este backend
 
 function App() {
